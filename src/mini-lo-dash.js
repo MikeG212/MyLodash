c0c769e98db359b4ddda83df1cb618c2dfbbab05
Mike Gee <40614634+MikeG212@users.noreply.github.com>
2019-12-23 10:38:31 -0800

    Implement lodash array methods

--- src/mini-lo-dash.js
+++ src/mini-lo-dash.js
@@ -2,8 +2,66 @@
     var _ = root._ = (root._ || {});
 
     // start writing your code here
-    // _.each = function (array, callback) {
-    //     // implementation!
-    // };
+    _.each = function (array, callback) {
+        for (var i = 0; i < array.length; i++) {
+            callback(array[i]);
+        }
+    };
+
+    _.any = function (array, callback) {
+        for (var i = 0; i <array.length; i++) {
+            if (callback(array[i])) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    _.map = function (array, callback) {
+        var resultsArray = [];
+        for (var i = 0; i < array.length; i++) {
+            resultsArray.push(callback(array[i]))
+        }
+        return resultsArray;
+    }
+
+    _.select= function (array, callback) {
+        var resultsArray = [];
+        for (var i = 0; i < array.length; i++) {
+            if (callback(array[i])) {
+                resultsArray.push(array[i]);
+            }
+        }
+        return resultsArray;
+    }
+
+    _.reject = function (array, callback) {
+        var resultsArray = [];
+        for (var i = 0; i < array.length; i++) {
+            if (!callback(array[i])) {
+                resultsArray.push(array[i]);
+            }
+        }
+        return resultsArray;
+    }
+
+    _.reduce = function (array, callback, accumulator) {
+        var accumulator = accumulator || array.shift();
+        for (var i = 0; i < array.length; i++) {
+            accumulator = callback(accumulator, array[i]);
+        }
+        return accumulator;
+    }
+
+    _.sample = function (array) {
+        var randomIndex = Math.floor((Math.random() * array.length));
+        return array[randomIndex];
+    }
+
+
+
+
+
+
 
 })(window);
